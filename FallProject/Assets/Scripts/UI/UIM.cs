c87bd9072512c1b202957dd2a8abf84708299cf9
Ryan Massia <ryanmassia6@gmail.com>
2020-08-25 10:29:28 -0400

    added prefabs and UI Manager

---
+++ FallProject/Assets/Scripts/UI/UIM.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIM : MonoBehaviour
+{
+    private static UIM instance = null;
+    //public static UIM inst { get { return instance; } }
+
+    public GameObject Shop;
+    public GameObject Stats;
+    public GameObject Inventory;
+    public GameObject Settings;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void loadWindow(GameObject Panel)
+    {
+        Panel.SetActive(true);
+    }
+
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
