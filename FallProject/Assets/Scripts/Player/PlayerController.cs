cd83c20f5ccf30ff0173a4a1c307601d204204e5
ACollings01 <alexandercollings@protonmail.com>
2020-08-25 16:10:36 -0400

    Designed a tutorial level layout

---
+++ FallProject/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject playerModel;
+	[SerializeField]
+	private GameObject playerCamera;
+
+	[SerializeField]
+	private float moveSpeed = 0.1f;
+
+	private void playerMovement()
+	{
+		if (Input.GetKey("d"))
+		{
+			playerModel.transform.position = new Vector3(playerModel.transform.position.x + moveSpeed, playerModel.transform.position.y, playerModel.transform.position.z);
+		}
+
+		if (Input.GetKey("a"))
+		{
+			playerModel.transform.position = new Vector3(playerModel.transform.position.x - moveSpeed, playerModel.transform.position.y, playerModel.transform.position.z);
+		}
+	}
+
+	private void Update()
+	{
+		playerMovement();
+	}
+}
