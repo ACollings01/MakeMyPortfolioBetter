4b327a1e75a739b5dfa90dce985b869d827d5e0c
Ryan Massia <ryanmassia6@gmail.com>
2020-08-28 16:07:26 -0400

    added joystick, movement works on x axis

---
+++ FallProject/Assets/Scripts/Player/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    protected Joystick joystick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody2D>();
+
+        rigidbody.velocity = new Vector2(joystick.Horizontal * 10.0f, rigidbody.velocity.y);
+    }
+}
